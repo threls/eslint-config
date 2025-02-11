075ab66d6eb6e0079a57615e2ea762bed29bdf57
ghili <gary@threls.com>
2025-02-11 15:32:03 +0100

    Initial commit

---
+++ react-native.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    "plugin:jsx-a11y/recommended",
+    "standard",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react-native/all",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier", "jsx-a11y"],
+  ignorePatterns: ["vite-env.d.ts", "node_modules/", "dist/"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/no-unescaped-entities": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-require-imports": "off",
+    "prettier/prettier": "error",
+  },
+};
